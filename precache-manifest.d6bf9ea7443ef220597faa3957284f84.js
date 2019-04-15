d1ef1974970f9bffa64da76343eb64f0b0f41015
Daniel Páez <danielpaezsw66@gmail.com>
2019-04-14 22:50:46 -0400

    Updates

---
+++ precache-manifest.d6bf9ea7443ef220597faa3957284f84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "be66f79bb96d97b4ac7b3fbaa3d146d9",
+    "url": "/patytime/static/media/imagen-outfit-daniel-paez-tomar-nader.be66f79b.jpeg"
+  },
+  {
+    "revision": "26b9e06b3269f5b3618b",
+    "url": "/patytime/static/js/runtime~main.5c79a741.js"
+  },
+  {
+    "revision": "241f70dda237d5f21668",
+    "url": "/patytime/static/js/main.5dc9f4ad.chunk.js"
+  },
+  {
+    "revision": "cf7b97960d8aaa40c18e",
+    "url": "/patytime/static/js/2.c2f79bd0.chunk.js"
+  },
+  {
+    "revision": "241f70dda237d5f21668",
+    "url": "/patytime/static/css/main.73989ef0.chunk.css"
+  },
+  {
+    "revision": "86b758dac1d043ff2b8c3e2e958941df",
+    "url": "/patytime/index.html"
+  }
+];
